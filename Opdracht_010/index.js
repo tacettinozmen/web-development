ae885bb2552157922caab4c3fc9eea96b09a3d81
tacettinozmen <90001591+tacettinozmen@users.noreply.github.com>
2021-09-18 18:17:25 +0200

    Javascript1

---
+++ Opdracht_010/index.js
@@ -0,0 +1,14 @@
+console.log("Hello Winc Academy")
+
+let variabele = 'if'
+
+console.log(variabele)
+
+let optel = 5 % 2
+console.log(optel)
+
+let leeftijd = "43"
+
+console.log(leeftijd)
+
+console.log(typeof leeftijd);
