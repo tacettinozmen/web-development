e4cc99af6ae29cacacf49a4d40271f67ef29dd25
tacettinozmen <90001591+tacettinozmen@users.noreply.github.com>
2021-09-19 19:56:34 +0200

    Opdracht 2 Uitsmijter

---
+++ Opdracht2/index.js
@@ -0,0 +1,27 @@
+// De uitsmijter
+
+
+const age = 18;
+const isFemale = true;
+const driverStatus = 'BOB';
+
+if (isFemale) {
+    console.log("U voldoet ladiesnight!");
+}
+
+else if (age >= 18) {
+    console.log("U voldoet aan de leeftijd!");
+}
+
+else if (driverStatus = 'BOB') {
+    console.log("U mag ook rijden");
+
+}
+
+else {
+    console.log("U voldoet niet aan de leeftijd en ladiesnight; u mag niet naar binnen.");
+}
+
+
+
+
