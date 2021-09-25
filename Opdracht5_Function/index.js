6349c3854aca85dad3625d467da5bc3aad043a6e
tacettinozmen <90001591+tacettinozmen@users.noreply.github.com>
2021-09-25 20:57:29 +0200

    Opdracht5 Funtions

---
+++ Opdracht5_Function/index.js
@@ -0,0 +1,29 @@
+//* Pizza making
+
+const pizzaMaker = function () {
+
+    console.log("P i z z a M a k e r");
+    console.log("====================");
+    console.log("Kneed de deeg");
+    console.log("Maak de pizzabodem");
+    console.log("Doe beleg op pizzabodem");
+    console.log("////////////////////////");
+    console.log("");
+}
+
+const sushiMaker = function () {
+
+    console.log("S u s h i M a k e r");
+    console.log("====================");
+    console.log("Maak de blade schoon");
+    console.log("Rol vis in bladen");
+    console.log("Snij de sushi");
+    console.log("////////////////////////");
+    console.log("");
+}
+
+console.log("Order 1"), sushiMaker();
+console.log("Order 2"), sushiMaker();
+console.log("Order 3"), sushiMaker();
+console.log("Order 1"), pizzaMaker();
+console.log("Order 2"), pizzaMaker();
