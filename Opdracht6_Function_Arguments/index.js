0168c2e234669dcd395303cc2f7a7ebaf4205a8b
tacettinozmen <90001591+tacettinozmen@users.noreply.github.com>
2021-09-28 21:10:50 +0200

    Opdracht6_Function_Arguments

---
+++ Opdracht6_Function_Arguments/index.js
@@ -0,0 +1,14 @@
+//* Wall painter
+
+const wallPainter = function (color, direction) {
+
+
+    console.log("The" + direction + "wall has been painted " + color);
+
+}
+
+const color = 'green';
+const direction = 'north';
+
+
+wallPainter(color);
