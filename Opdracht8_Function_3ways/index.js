aa985df62a3f76d00da314dd7efe8e4cc0cf4af2
tacettinozmen <90001591+tacettinozmen@users.noreply.github.com>
2021-10-02 21:23:14 +0200

    Opdracht8_Function_3ways

---
+++ Opdracht8_Function_3ways/index.js
@@ -0,0 +1,34 @@
+/* 
+
+// 3 ways
+
+// Function declaration
+
+function add(number1, number2) {
+    return number1 + number2;
+    
+  }
+  
+   
+
+  console.log(add(7, 7));
+
+
+// Function expression
+
+const add = function (number1, number2) {
+  return number1 + number2;
+};
+
+console.log(add(7, 7));
+
+*/
+
+// arrow function
+
+const add = (number1, number2) => {
+    return number1 + number2;
+  };
+  
+console.log(add(7, 7));
+
