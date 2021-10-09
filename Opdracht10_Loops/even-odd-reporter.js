7c1d8ed14fd8e36e4440468e0fcc908957f32445
tacettinozmen <90001591+tacettinozmen@users.noreply.github.com>
2021-10-09 19:11:43 +0200

    Opdracht10_Loops

---
+++ Opdracht10_Loops/even-odd-reporter.js
@@ -0,0 +1,18 @@
+
+for (let i = 0; i <= 20; i++) 
+
+if (i % 2 == 0) {
+    console.log(`Is ${i} even`);
+}
+else {
+
+    console.log(`Is ${i} odd`);
+}
+
+  
+  // Expected output:
+  // 1 is odd 
+  // 2 is even 
+  // 3 is odd 
+  // 4 is even 
+  // ...
---
+++ Opdracht10_Loops/multiplication-tables.js
@@ -0,0 +1,19 @@
+var table = 5;
+var i = 1;
+
+for(i=1; i <= 10; i++)
+ document.write("<br>"+i+" * "+table+" = " +(i * table));
+
+
+
+
+// 1 x 1 = 1 
+// 1 x 2 = 2 
+// 1 x 3 = 3 
+// etc...
+
+// ...
+// 9 x 1 = 9 
+// 9 x 2 = 18 
+// 9 x 3 = 27
+// ...etc 
